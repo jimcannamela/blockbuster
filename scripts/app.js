71085f15e6d994404d00f5491b8410d1947a0c91
Jim Cannamela <jim.cannamela@outlook.com>
2023-05-09 10:34:29 -0500

    initial psuedocode completed

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+// This is our javascript there are otheres like it, but this is ours!!!
+/*
+
+
+1. Get the data from the input search bar
+
+2. need a event listener for click
+
+3. fetch data json
+
+4. .then to return data from json
+
+5. add items to result list
+    include links to details page
+    need second event listener on click of moview title or poster pass id of movie or series to movie page
+
+
+For Movie Page 
+
+on load use passed data to call api for movie details.
+
+For carousel page
+
+on load call api for new titles
+
+*/
+
+
---
+++ scripts/whatsnew.js
@@ -0,0 +1,8 @@
+/*   Psuedo code for what's new details
+
+call API to retrieve current releases
+fetch
+then
+format html elements
+
+*/
