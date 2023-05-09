c3677bf73e45cb7bcd233d5adb90782eaca70a41
Jim Cannamela <jim.cannamela@outlook.com>
2023-05-09 11:17:08 -0500

    Completed retrieving test data

---
+++ scripts/Ed Wood.js
@@ -0,0 +1,53 @@
+  const Person1Data =  
+  {
+  "title_results": [
+    {
+      "resultType": "title",
+      "id": 1114888,
+      "name": "Ed Wood",
+      "type": "movie",
+      "year": 1994,
+      "imdb_id": "tt0109707",
+      "tmdb_id": 522,
+      "tmdb_type": "movie"
+    },
+    {
+      "resultType": "title",
+      "id": 1659742,
+      "name": "Ed Wood: Look Back in Angora",
+      "type": "movie",
+      "year": 1994,
+      "imdb_id": "tt0144185",
+      "tmdb_id": 154028,
+      "tmdb_type": "movie"
+    },
+    {
+      "resultType": "title",
+      "id": 1665586,
+      "name": "Ed Wood: The Theremin",
+      "type": "movie",
+      "year": 2004,
+      "imdb_id": "tt1852950",
+      "tmdb_id": 683850,
+      "tmdb_type": "movie"
+    }
+  ],
+  "people_results": [
+    {
+      "resultType": "person",
+      "id": 79619730,
+      "name": "Ed Wood",
+      "main_profession": "",
+      "imdb_id": "",
+      "tmdb_id": 2390756
+    },
+    {
+      "resultType": "person",
+      "id": 710125611,
+      "name": "Ed Wood",
+      "main_profession": "cinematographer",
+      "imdb_id": "nm7903892",
+      "tmdb_id": 2901757
+    }
+  ]
+};
